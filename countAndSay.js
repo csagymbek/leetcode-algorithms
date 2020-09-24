a97936eda00adb6e740081849806cc55f25d4126
Serik Sagymbek <csagymbek@gmail.com>
2020-09-24 15:19:59 -0700

    first commit

---
+++ countAndSay.js
@@ -0,0 +1,38 @@
+// 38. Count and Say
+// The count-and-say sequence is the sequence of integers with the first five terms as following:
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+// 1 is read off as "one 1" or 11.
+// 11 is read off as "two 1s" or 21.
+// 21 is read off as "one 2, then one 1" or 1211.
+
+// Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit. Note: Each term of the sequence of integers will be represented as a string.
+function countAndSay(n){
+    var str = '1';
+    for (var i=1; i < n; i++) {
+        var strArray = str.split('');
+        str ='';
+        var count = 1;
+        // Loop through current nth level line
+        for (var j=0; j < strArray.length; j++) {
+            // Next digit is different
+            if (strArray[j] !== strArray[j+1]) {
+                // Go to next non-matching digit
+                str += count + strArray[j];
+                count = 1;
+            }
+            else {
+                count++;
+            }
+        }
+    }
+    return str;
+}
+console.log(countAndSay(1));//"1"
+console.log(countAndSay(2));//"11"
+console.log(countAndSay(3));//"21"
+console.log(countAndSay(4));//"1211"
+console.log(countAndSay(5));//"111221"
--- searchInsert.js
+++ searchInsert.js
@@ -1,6 +1,5 @@
 // 35. Search Insert Position
-// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-// You may assume no duplicates in the array.
+// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order. You may assume no duplicates in the array.
 function searchInsert(nums, target){
     for(var i = 0; i < nums.length; i++){
         if(target <= nums[i]){
@@ -11,7 +10,7 @@ function searchInsert(nums, target){
 }
 
 function searchInsert(nums, target){
-    var i = 0; 
+    var i = 0;
     var j = nums.length;
     while(i <= j){
         var mid = Math.floor((i + j) / 2);
@@ -29,4 +28,5 @@ console.log(searchInsert([1,3,5,6], 5));//2
 console.log(searchInsert([1,3,5,6], 2));//1
 console.log(searchInsert([1,3,5,6], 7));//4
 console.log(searchInsert([1,3,5,6], 0));//0
+console.log(searchInsert([]));//0
 
